c04f36ee9dd8b134a864f1d930e20e82f0eefd6c
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-10-28 20:46:35 +0300

    Added important packages to our project

---
+++ lib/islami_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class IslamiApp extends StatelessWidget {
+  const IslamiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Islami App',
+    );
+  }
+}
