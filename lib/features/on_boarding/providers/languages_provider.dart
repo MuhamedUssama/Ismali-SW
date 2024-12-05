123c9e55766774f5976c9ce25622ec59c92fab65
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-12-05 03:24:01 +0200

    Saved isClicked and language code in shared preferences

--- lib/features/on_boarding/providers/languages_provider.dart
+++ lib/features/on_boarding/providers/languages_provider.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/core/cache/shared_preferences_helper.dart';
+import 'package:islami_app/core/utils/app_constants.dart';
 
 class LanguagesProvider extends ChangeNotifier {
   bool isClicked = true;
   String languageCode = 'en';
 
+  LanguagesProvider() {
+    getSavedLanguage();
+    getIsClicked();
+  }
+
   void onSwitchLanguage(String selectedLanguageCode) {
     isClicked = !isClicked;
     if (selectedLanguageCode == languageCode) return;
     languageCode = selectedLanguageCode;
+    saveLanguage(languageCode);
+    notifyListeners();
+  }
+
+  Future<void> saveLanguage(String lang) async {
+    await SharedPreferencesHelper.saveData(
+      key: AppConstants.languageKey,
+      value: lang,
+    );
+  }
+
+  Future<void> getSavedLanguage() async {
+    languageCode = await SharedPreferencesHelper.getString(
+      key: AppConstants.languageKey,
+    );
+
+    if (languageCode.isEmpty) {
+      languageCode = 'en';
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> saveIsClicked(bool isClicked) async {
+    await SharedPreferencesHelper.saveData(
+      key: AppConstants.isClickedKey,
+      value: isClicked,
+    );
+  }
+
+  Future<void> getIsClicked() async {
+    isClicked = await SharedPreferencesHelper.getBool(
+      key: AppConstants.isClickedKey,
+    );
+
     notifyListeners();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'core/cache/shared_preferences_helper.dart';
 import 'features/on_boarding/providers/languages_provider.dart';
 import 'islami_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferencesHelper.init();
+
   runApp(ChangeNotifierProvider(
     create: (context) => LanguagesProvider(),
     child: const IslamiApp(),
