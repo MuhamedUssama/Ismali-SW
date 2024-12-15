57c837e24bbab61f8cec6c248b1f381398cdf12e
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-12-15 23:58:17 +0200

    Finised Sebha tab logic and ui

--- lib/features/home/tabs/sebha_tab/sebha_tab.dart
+++ lib/features/home/tabs/sebha_tab/sebha_tab.dart
@@ -3,59 +3,75 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:islami_app/config/theme/app_text_styles.dart';
 import 'package:islami_app/core/utils/app_assets.dart';
+import 'package:provider/provider.dart';
+
+import 'provider/sebha_tab_provider.dart';
 
 class SebhaTab extends StatelessWidget {
   const SebhaTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 25.w, vertical: 16.h),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            AppLocalizations.of(context)!.bearishDescribtion,
-            style: AppTextStyles.text36WhiteBold,
-          ),
-          SizedBox(height: 16.h),
-          Stack(
-            alignment: AlignmentDirectional.topCenter,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 40.w),
-                child:
-                    Image.asset(AppImages.sebhaTail, width: 73.w, height: 86.h),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 75.h),
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Image.asset(
-                      AppImages.sebhaBody,
-                      width: 379.w,
-                      height: 381.h,
-                    ),
-                    Column(
-                      children: [
-                        const Text(
-                          "سبحان الله",
-                          style: AppTextStyles.text36WhiteBold,
-                        ),
-                        SizedBox(height: 10.h),
-                        const Text(
-                          "30",
-                          style: AppTextStyles.text36WhiteBold,
-                        ),
-                      ],
-                    )
-                  ],
+    return ChangeNotifierProvider(
+      create: (context) => SebhaTabProvider(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 25.w, vertical: 16.h),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              AppLocalizations.of(context)!.bearishDescribtion,
+              style: AppTextStyles.text36WhiteBold,
+            ),
+            SizedBox(height: 16.h),
+            Stack(
+              alignment: AlignmentDirectional.topCenter,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 40.w),
+                  child: Image.asset(AppImages.sebhaTail,
+                      width: 73.w, height: 86.h),
                 ),
-              ),
-            ],
-          )
-        ],
+                Padding(
+                  padding: EdgeInsets.only(top: 75.h),
+                  child: Consumer<SebhaTabProvider>(
+                    builder: (context, provider, child) {
+                      return Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: provider.onZkrClicked,
+                            child: Transform.rotate(
+                              angle: provider.angle,
+                              child: Image.asset(
+                                AppImages.sebhaBody,
+                                width: 379.w,
+                                height: 381.h,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                provider.azkar[provider.index],
+                                style: AppTextStyles.text36WhiteBold,
+                              ),
+                              SizedBox(height: 10.h),
+                              Text(
+                                provider.counter.toString(),
+                                style: AppTextStyles.text36WhiteBold,
+                              ),
+                            ],
+                          )
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
