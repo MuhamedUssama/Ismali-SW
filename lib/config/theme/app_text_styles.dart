194c656018fcd01cbb3f1213ef742982b1632720
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-12-15 23:50:21 +0200

    Finished Sebha tab ui

--- lib/config/theme/app_text_styles.dart
+++ lib/config/theme/app_text_styles.dart
@@ -16,8 +16,43 @@ class AppTextStyles {
     fontFamily: 'Janna-LT',
   );
 
+  static const TextStyle text16WhiteBold = TextStyle(
+    fontSize: 16,
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Janna-LT',
+  );
+
+  static const TextStyle text20WhiteBold = TextStyle(
+    fontSize: 20,
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Janna-LT',
+  );
+
+  static const TextStyle text36WhiteBold = TextStyle(
+    fontSize: 36,
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Janna-LT',
+  );
+
   static const TextStyle text16GoldBold = TextStyle(
     fontSize: 16,
+    color: AppColors.gold,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Janna-LT',
+  );
+
+  static const TextStyle text20GoldBold = TextStyle(
+    fontSize: 20,
+    color: AppColors.gold,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Janna-LT',
+  );
+
+  static const TextStyle text24GoldBold = TextStyle(
+    fontSize: 24,
     color: AppColors.gold,
     fontWeight: FontWeight.bold,
     fontFamily: 'Janna-LT',
@@ -30,37 +65,9 @@ class AppTextStyles {
     fontFamily: 'Janna-LT',
   );
 
-  static const TextStyle text16WhiteBold = TextStyle(
-    fontSize: 16,
-    color: AppColors.white,
-    fontWeight: FontWeight.bold,
-    fontFamily: 'Janna-LT',
-  );
-
   static const TextStyle text20BlackBold = TextStyle(
     fontSize: 20,
     color: AppColors.black,
-    fontWeight: FontWeight.bold,
-    fontFamily: 'Janna-LT',
-  );
-
-  static const TextStyle text20GoldBold = TextStyle(
-    fontSize: 20,
-    color: AppColors.gold,
-    fontWeight: FontWeight.bold,
-    fontFamily: 'Janna-LT',
-  );
-
-  static const TextStyle text20WhiteBold = TextStyle(
-    fontSize: 20,
-    color: AppColors.white,
-    fontWeight: FontWeight.bold,
-    fontFamily: 'Janna-LT',
-  );
-
-  static const TextStyle text24GoldBold = TextStyle(
-    fontSize: 24,
-    color: AppColors.gold,
     fontWeight: FontWeight.bold,
     fontFamily: 'Janna-LT',
   );
--- lib/features/home/tabs/sebha_tab/sebha_tab.dart
+++ lib/features/home/tabs/sebha_tab/sebha_tab.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami_app/config/theme/app_text_styles.dart';
+import 'package:islami_app/core/utils/app_assets.dart';
 
 class SebhaTab extends StatelessWidget {
   const SebhaTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25.w, vertical: 16.h),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            AppLocalizations.of(context)!.bearishDescribtion,
+            style: AppTextStyles.text36WhiteBold,
+          ),
+          SizedBox(height: 16.h),
+          Stack(
+            alignment: AlignmentDirectional.topCenter,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 40.w),
+                child:
+                    Image.asset(AppImages.sebhaTail, width: 73.w, height: 86.h),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 75.h),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Image.asset(
+                      AppImages.sebhaBody,
+                      width: 379.w,
+                      height: 381.h,
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          "سبحان الله",
+                          style: AppTextStyles.text36WhiteBold,
+                        ),
+                        SizedBox(height: 10.h),
+                        const Text(
+                          "30",
+                          style: AppTextStyles.text36WhiteBold,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
