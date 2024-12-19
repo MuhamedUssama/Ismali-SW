1b90c052b679c2b74817f3cb699fb3993291ddd5
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-12-19 03:18:12 +0200

    Added language support to RadioListWidgets

--- lib/features/home/tabs/radio_tab/widgets/radio_list_widgets.dart
+++ lib/features/home/tabs/radio_tab/widgets/radio_list_widgets.dart
@@ -3,6 +3,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:islami_app/config/theme/app_colors.dart';
 import 'package:islami_app/features/home/tabs/radio_tab/api/radio_api_manager.dart';
 import 'package:islami_app/features/home/tabs/radio_tab/models/radio/radios_model.dart';
+import 'package:islami_app/features/on_boarding/providers/languages_provider.dart';
+import 'package:provider/provider.dart';
 
 import 'custom_radio_item.dart';
 
@@ -11,8 +13,16 @@ class RadioListWidgets extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var languagesProvider = Provider.of<LanguagesProvider>(context);
+    String language = 'ar';
+    if (languagesProvider.languageCode == 'en') {
+      language = 'eng';
+    } else {
+      language = 'ar';
+    }
+
     return FutureBuilder(
-      future: RadioApiManager.getRadios(),
+      future: RadioApiManager.getRadios(language: language),
       builder: (context, snapshot) {
         List<Radios> radios = snapshot.data?.radios ?? [];
 
